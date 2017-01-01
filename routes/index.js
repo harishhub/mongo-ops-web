ea40426c1e7944640cb5fdf5bb3a938e80bb7878
harishhub_8310 <hk8049@gmail.com>
2017-01-02 01:47:45 +0530

    added route for mongorestore responding back with a response FUNCTIONALTY PENDING

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,17 @@
 "use strict"
 var path=require('path');
 var mongoutil = require(path.resolve("./lib/index"));
+
 module.exports = function(app){
+
 	app.get("/",function(req,res){
 		
-		/*var query=[];
-		var options=req.body;
-		if(options.allDatabases)
-			query.push('--host');
-		else if(options.databaseName){
-			query.push('--db')
-			query.push(options.databaseName)
-		}
-		if(options.collectionName)
-
-		query.push()
-
-				var  = ['--db', options.database, '--collection', "values", '--out', '/home/harish/dump/'];
-				*/
-
-
-
 		mongoutil.mongodump();
 		res.status(200).json("started");
-	})
+	});
+
+	app.get("/mongorestore",function(req,res){
+		var restoreRes = mongoutil.mongorestore();
+		res.status(200).json(restoreRes);
+	});
 };
